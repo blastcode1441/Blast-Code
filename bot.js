e82f4b74b161c5dbd2309bc5274453b907319479
blastcode1441 <44486185+blastcode1441@users.noreply.github.com>
2018-10-26 10:44:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -325,6 +325,7 @@ client.on('message', message => {
 
         
 client.on('message', message => {
+var prefix = "!"
     if(message.content.startsWith(prefix + 'new')) {
         let args = message.content.split(' ').slice(1).join(' ');
         let support = message.guild.roles.find("name","Support Team");
@@ -400,6 +401,18 @@ client.on('message', message => {
  
            
     }
+});
+
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const stewart = member.guild.channels.find("name", "welcome");
+     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
 });
 
 client.login(process.env.BOT_TOKEN);

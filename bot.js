177d79289ab4daaba76772ae6d2a1fa6cc4006a5
blastcode1441 <44486185+blastcode1441@users.noreply.github.com>
2018-10-26 19:01:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -514,16 +514,6 @@ msg.channel.send(`**Bot InviteURL : ** https://discordapp.com/oauth2/authorize?c
           
 
 
-            if (message.content.startsWith(prefix + "!bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
+
 
 client.login(process.env.BOT_TOKEN);

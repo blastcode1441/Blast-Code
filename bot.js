f9bc892c172f307e3198f5c6eb832b9135bb83cb
blastcode1441 <44486185+blastcode1441@users.noreply.github.com>
2018-10-27 00:34:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -724,15 +724,7 @@ var prefix = "!";
 });
 
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const member = member.guild.channels.find("name", "✵-「public-chat");
-     member.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-  }); 
-});
+
 
 
 client.login(process.env.BOT_TOKEN);

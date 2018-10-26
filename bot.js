1c466dadc042ef5ec508da5a183bfb3ddec09015
blastcode1441 <44486185+blastcode1441@users.noreply.github.com>
2018-10-26 13:25:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -502,7 +502,16 @@ var prefix = "!"
 });
 
 
-
+client.on('message', msg => {
+    if(msg.content.startsWith('!link')) {
+    if(msg.channel.type === 'dm') return;
+const user = msg.mentions.users.first();
+if(!user) return msg.channel.send('``' + '**قم بتحديد بوت**' + '``')
+if(!user.bot) return msg.reply('\`منشن بوت\`');
+msg.channel.send(`**Bot InviteURL : ** https://discordapp.com/oauth2/authorize?client_id=${user.id}&scope=bot&permissions=384064`)
+    }
+});          
+          
 
 
 

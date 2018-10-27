057017f3a1662c8c18300ea7234ec4090ca6c1d9
blastcode1441 <44486185+blastcode1441@users.noreply.github.com>
2018-10-27 10:05:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -521,4 +521,22 @@ client.channels.find('name', "log").send({embed : unmuteembed});
 
 
 });
+
+
+ client.on('message', message => {
+	       var prefix = "!";
+            if (message.content.startsWith(prefix + "bot")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField(' السيرفرات🌐',`[${client.guilds.size}]  `)
+.addField(' الاعضاء👥 ',` [${client.users.size}] `)
+.addField('الرومات📚 ',`[${client.channels.size}]`) 
+.addField(' البنق🚀 ',`[${Date.now() - message.createdTimestamp}]`) 
+.addField('مصمم  + صاحب البوت ',`! 1 NoNo`)
+.setColor('#7d2dbe')
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
 client.login(process.env.BOT_TOKEN);
